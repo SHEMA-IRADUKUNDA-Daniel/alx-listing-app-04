a2e353ab31d03fc4be3114e432553c0b40b603e4
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-24 02:04:04 +0200

    improving the layout of the app

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,4 @@
+import React, { ReactNode } from "react";
 // export type ButtonPros = { children: React.ReactNode; className?: string };
 export interface ButtonPros {
   children: React.ReactNode;
@@ -8,3 +9,30 @@ export interface CardProps {
   children: React.ReactNode;
   className?: string;
 }
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+export interface PillProps {
+  label: string;
+  onClick?: () => void;
+  selected?: boolean;
+}
+
+export type LayoutProps = {
+  children: ReactNode;
+};
